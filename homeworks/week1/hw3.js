c4838a545275cc2e3415192ad10098b5adccb64a
Jean Pang <jeanpang@gmail.com>
2018-02-02 18:28:45 +0800

    Finished hw3

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,29 @@
 export const isPrime = (n) => {
-  return false;
-}
+  if (!Number.isInteger(n))
+  {
+      return false;
+  };
+
+  if (n<2){
+      return false;
+  };
+
+  if (n===2){
+      return true;
+  }
+  else if (n%2===0){
+      return false;
+  };
+
+  if (n===[3,5,7,9]){
+      for(i=3; i<11; i+2){
+          if (n%i===0){
+              return false;}
+          else 
+              return true;
+          }; 
+      };
+  return true;
+  };
+
+console.log (isPrime(98))
